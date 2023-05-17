21991e4c4ee0776f0f20edd9a104f9fa5d7eb5e6
CheFly1116 <sam4363@naver.com>
2023-05-17 22:44:18 +0900

    openai 함수 부분 추가

--- backend/mainApp/controllers/example_controllers.py
+++ backend/mainApp/controllers/example_controllers.py
@@ -7,6 +7,10 @@ example_bp = Blueprint(name='example',
                        import_name=__name__,
                        url_prefix='/example')
 
+openai_bp = Blueprint(name='answer',
+                      import_name=__name__,
+                      url_prefix='/answer')
+
 @example_bp.route('/', methods=['GET'])
 def example_route() -> str:
     data = 'hello_world'
@@ -16,4 +20,10 @@ def example_route() -> str:
 @example_bp.route('/<int:user_number>', methods=['GET'])
 def example_route_add_param(user_number: int) -> str:
     result = example_services.example_route_add_param(user_number)
+    return jsonify(result=result)
+
+@openai_bp.route('/', methods=['GET']) # openai 답변 생성 요청 들어갈 곳
+def answer_route() -> str:
+    data = 'hello_world'
+    result = example_services.answer_route(data=data)
     return jsonify(result=result)
--- backend/mainApp/routes.py
+++ backend/mainApp/routes.py
@@ -1,5 +1,6 @@
-from .controllers.example_controllers import example_bp
+from .controllers.example_controllers import example_bp, openai_bp
 
 def routes_list(app):
     app.register_blueprint(example_bp)
+    app.register_blueprint(openai_bp)
     return app
